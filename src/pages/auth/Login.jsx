1d6081eb7d774a7ad58b3430c276e30ede994b70
Chinghaogit config --global user.email chinghao.hsu@hva.nlgit config --global user.name <chinghaoh@hotmail.nl>
2026-06-01 15:54:52 +0200

    test: logs

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -27,6 +27,9 @@ export default function Login() {
         method: 'POST',
         body: { email, password },
       })
+      console.log('handleSubmit email:', email) 
+      console.log('handleSubmit password:', password)
+      console.log('handleSubmit test:', "test")
       console.log('handleSubmit data:', data)
       login(data)
       navigate(data.hasCompletedOnboarding ? '/dashboard' : '/onboarding')
